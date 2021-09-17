91f9a95c15dde544200fffe6c57955ee01bd7cde
Zhengiskan <64011433@kmitl.ac.th>
2021-09-17 22:07:18 +0700

    Update main.c

--- atm/main.c
+++ atm/main.c
@@ -4,8 +4,8 @@
 int main() {
     printf("Hello, World!\n");
     printf("Test GitHub\n");
-    printf("I love pizza");
-    printf("Test form, i also love pepporoni pizza");
-    printf("I bet Jesus loves pizza too");
+    printf("I love pizza\n");
+    printf("Test form, i also love pepporoni pizza\n");
+    printf("I bet Jesus loves pizza too\n");
     return 0;
 }
